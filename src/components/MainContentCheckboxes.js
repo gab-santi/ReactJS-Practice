ca239d5b79eddb2b304e5045d4262c344719cd6c
gab-santi <gabbysantiago96@yahoo.com>
2019-10-22 20:44:30 +0800

    Added props to To-do items

--- src/components/MainContentCheckboxes.js
+++ src/components/MainContentCheckboxes.js
@@ -1,13 +1,15 @@
 import React from "react"
 import TodoItem from "./TodoItem"
+import todosData from "./todosData"
 
 function MainContentCheckboxes() {
+    const checklistData = todosData.map(
+        item => <TodoItem key={item.id} completed={item.completed} text={item.text} />
+    )
+
     return (
         <div className="todo-list">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {checklistData}
         </div>
     )
 }
