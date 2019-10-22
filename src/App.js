cc2c154dbc558f4660fda8ee865edf28d7e79f55
gab-santi <gabbysantiago96@yahoo.com>
2019-10-22 16:34:27 +0800

    Added props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import MainContentCheckboxes from "./components/MainContentCheckboxes"
 import Header from "./components/Header"
+import ContactCard from "./components/ContactCard"
 
 /* function App() {
   const firstName = "Gab"
@@ -51,6 +52,24 @@ function App() {
     <div>
       <Header />
       <MainContentCheckboxes />
+      
+      <ContactCard 
+        // class approach
+        contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "123123123", email: "mr.whiskaz@catnap.meow"}}
+      />
+
+      <ContactCard 
+        contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "123123123", email: "mr.whiskaz@catnap.meow"}}
+      />
+
+      <ContactCard 
+        contact = {{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "123123123", email: "mr.whiskaz@catnap.meow"}}
+      />
+
+      <ContactCard 
+        contact = {{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "123123123", email: "mr.whiskaz@catnap.meow"}}
+      />
+
     </div>
   )
 }
